0c5df4afdd0734e1ce5a5ae85b1da81e87f5b388
Acochisse <1819@holbertonschool.com>
2023-02-20 11:56:07 -0600

    adding readme and 0

---
+++ 0x01-dart-function/0-hellofunc.dart
@@ -0,0 +1,10 @@
+// Write a dart function helloFunction() that accepts one argument String and print Hello Holberton from dart
+
+// Function prototype :void helloFunction(String str)
+
+void helloFunction(String str) {
+  print("Hello ${str} from dart");
+}
+void main() {
+  helloFunction("Holberton");
+}
