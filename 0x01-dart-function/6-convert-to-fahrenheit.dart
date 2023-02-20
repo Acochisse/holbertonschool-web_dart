cc601b611425603dd1d957cfd397480d4d0cba8f
Acochisse <1819@holbertonschool.com>
2023-02-20 12:35:21 -0600

    .tostringfixed

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,9 +1,10 @@
 // function that converts a celsius temperature to fahrenheit. results should be rounded to 2 places
-List<double> convertToF(List<double> temperaturesInC) {
-    List<double> temperaturesInF = [];
-    for (double temp in temperaturesInC) {
-        temperaturesInF.add((temp * 9 / 5) + 32).toStringFixed(2);
+// use .toStringFixed(2) to round to 2 places in the function
 
+List<double> convertToF(List<double> temperaturesInC) {
+    List<double> temperatures = new ArrayList<>();
+    for (double temp : temperaturesInC) {
+        temperatures.add((temp * 9 / 5).toStringFixed(2) + 32);
     }
-    return temperaturesInF;
-    }
+    return temperatures;
+}
