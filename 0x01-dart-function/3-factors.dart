6f0b082a3dd4e593d86ef05a1bfd2551dff259ff
Acochisse <1819@holbertonschool.com>
2023-02-20 12:16:57 -0600

    op

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -5,11 +5,11 @@
 // If f <= 0 return 0
 
 int fact(int f) {
-  if (f == 1) {
-    return 1;
+    if (f == 1) {
+        return 1;
     } else if (f <= 0) {
         return 0;
-        } else {
-        return 1;
+    } else {
+        return f * fact(f - 1);
     }
 }
