e345c3ae60149fde2a935f1cb1a9ca9c4e60e8ef
Acochisse <1819@holbertonschool.com>
2023-02-20 12:15:59 -0600

    try again

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -4,19 +4,12 @@
 // If f == 1 the function return 1
 // If f <= 0 return 0
 
-include 'dart:math';
-
 int fact(int f) {
   if (f == 1) {
-    // Base case
     return 1;
-    // return 1;
     } else if (f <= 0) {
         return 0;
-        // return 0;
         } else {
         return 1;
-        // return 1;
     }
-    // return 0;
 }
