ac3b4db7d6d56dc1c23cc8c1632bbba122cec30e
Acochisse <1819@holbertonschool.com>
2023-02-20 11:57:46 -0600

    adding 1

---
+++ 0x01-dart-function/1-concat_strings.dart
@@ -0,0 +1,7 @@
+// Write a dart function concatStr() that accepts two arguments str1, str2 and return Holberton School
+
+// Function prototype : String concatStr(String str1, String str2)
+
+void concatStr(String& str1, String& str2) {
+    str1.concat(str2);
+}
