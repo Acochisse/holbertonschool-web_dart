9b00a443c26899cb2d79e206becd11f146e3c083
Acochisse <1819@holbertonschool.com>
2023-02-21 12:48:42 -0600

    changes

--- 0x02-dart-classes/0-class.dart
+++ 0x02-dart-classes/0-class.dart
@@ -3,9 +3,9 @@
 // That contain propertie called name
 // and a method called showName that return string representation of User “Hello name”
 class User {
-public:
-  String name;
+  String name = '';
+
   String showName() {
-    return "Hello " + name;
+    return 'Hello $name';
   }
 }
