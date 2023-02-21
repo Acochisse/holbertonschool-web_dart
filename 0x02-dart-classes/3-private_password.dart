5519be8276d37fa3fcf84dc66615ffb94ce60eae
Acochisse <1819@holbertonschool.com>
2023-02-21 13:11:59 -0600

    adding 3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,36 @@
+// Create a class called Password and give it a string property called password.
+
+// Add a method to Password calledisValid that returns true only
+
+// The length of password should be between 8 and 16.
+// password should contains Uppercase letters and lowercase letters
+// password should contains numbers .
+// toString :
+// Example password = "ShouldWorkf7ne"
+// Override the toString method of Password class to prints Your Password is: ShouldWorkf7ne.
+
+class Password {
+    String password = '';
+
+    Password({required this.password});
+    
+    bool isValid() {
+        return password.length >= 8 &&
+            password.length <= 16 &&
+            password.contains(RegExp(r'[A-Z]')) &&
+            password.contains(RegExp(r'[a-z]')) &&
+            password.contains(RegExp(r'[0-9]'));
+    }
+    
+    @override
+    String toString() {
+        return 'Your Password is: $password';
+    }
+    }
+
+void main() {
+    Password password = new Password();
+    if (password.isValid()) {
+        print(password);
+    }
+}
