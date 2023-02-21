2d5c68b41a7c2e9a0c665102f778715fea931ecc
Acochisse <1819@holbertonschool.com>
2023-02-21 13:05:44 -0600

    add required and map

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -9,8 +9,14 @@ class User {
 
   User({required this.name, required this.age, required this.height});
 
+ Map toJson() {
+    return {
+      'name': name, 'age': age, 'height': height,
+    };
+  }
+}
 
-  Map toJson() {
-    return '{ "name": "$name", "age": $age, "height": $height }';
-  }
-  }
+void main() {
+  final user = User(name: "Youssef", age: 25, height: 1.89);
+  print(user.toJson());
+}
