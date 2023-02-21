7b1138dbd21fb1324f89941ef609d8f747e1fd45
Acochisse <1819@holbertonschool.com>
2023-02-21 12:30:20 -0600

    rework

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -8,16 +8,18 @@ String longestUniqueSubstring(String str) {
     String longest = "";
     String current = "";
 
-    for (int i = 0; i < str.length(); i++) {
-        String c = str.substring(i, i + 1);
-        if (current.contains(c)) {
-            if (current.length() > longest.length()) {
-                longest = current;
-            }
-            current = c;
-        } else {
-            current += c;
-        }
+  for (int i = 0; i < str.length; ++i) {
+    for (int j = i; j < str.length; ++j) {
+      current = str.substring(i, j + 1);
+      if (current.length > longest.length && isUnique(current)) {
+        longest = current;
+      }
     }
-    return longest;
+  }
+
+  return longest;
+}
+
+bool isUnique(String str) {
+  return str == str.split('').toSet().join('');
 }
