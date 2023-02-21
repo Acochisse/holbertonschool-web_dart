4c2503a4c6c131ed3cad91eeabb83fa7c424fd3c
Acochisse <1819@holbertonschool.com>
2023-02-21 12:28:55 -0600

    rework

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -5,24 +5,19 @@
 // If multiple substrings tie in length, return the one which occurs first.
 
 String longestUniqueSubstring(String str) {
-    int n = str.length();
-    int res = 0; // result
-    int[] lastIndex = new int[256];
-    // Initialize all occurrence as -1
-    Arrays.fill(lastIndex, -1);
-    // Initialize start of current window
-    int i = 0;
-    // Move end of current window
-    for (int j = 0; j < n; j++) {
-        // Find the last index of str[j]
-        // Update i (starting index of current window)
-        // as maximum of current value of i and last
-        // index plus 1
-        i = Math.max(i, lastIndex[str.charAt(j)] + 1);
-        // Update result if we get a larger window
-        res = Math.max(res, j - i + 1);
-        // Update last index of j.
-        lastIndex[str.charAt(j)] = j;
+    String longest = "";
+    String current = "";
+
+    for (int i = 0; i < str.length(); i++) {
+        String c = str.substring(i, i + 1);
+        if (current.contains(c)) {
+            if (current.length() > longest.length()) {
+                longest = current;
+            }
+            current = c;
+        } else {
+            current += c;
+        }
     }
-    return res;
+    return longest;
 }
