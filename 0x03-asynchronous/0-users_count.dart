adf9387ea9a7d73de3ca9cb1a47c9a9438ab444f
Acochisse <1819@holbertonschool.com>
2023-02-22 14:29:13 -0600

    async

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,12 +1,8 @@
 // Create a function usersCount() that prints the number of users.
 
 // Function prototype : Future<void> usersCount() Gets the user count by calling the provided function fetchUsersCount(). which will simulate fetching data from an api.
-
-Future<void> usersCount() {
-    return fetchUsersCount();
+import '0-main.dart';
+Future<void> usersCount() async {
+  final int usersCount = await fetchUsersCount();
+  print('$usersCount');
 }
-
-Future<int> fetchUsersCount() => Future.delayed(
-      const Duration(seconds: 2),
-      () => 19,
-    );
