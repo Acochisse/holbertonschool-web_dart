0ee38255cca41ad26f01f93dc7d0ca2e07995c19
Acochisse <1819@holbertonschool.com>
2023-02-22 14:37:07 -0600

    adding 1

---
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -0,0 +1,10 @@
+// Create a function getUserID() that returns a future string representing the user’s ID.
+
+// Function prototype : Future<String> getUserId()
+
+// Gets the user data by calling the provided function fetchUserData() which will simulate fetching data from an api.
+import '1-main.dart';
+Future<String> getUserID() async {
+  final Map<String, dynamic> userData = await fetchUserData();
+  return userData['id'];
+}
